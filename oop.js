8f2cbe94bc81ae542415a4a27631ea1147556d6f
bola <bfagbowore@gmail.com>
2016-05-18 05:30:49 +0100

    created the function to  lists out each of the notes in the notes list

--- oop.js
+++ oop.js
@@ -12,3 +12,13 @@ function create(note_content){
 		notes.push(note_content);
 		return notes[];
 	}
+
+	function listNotes(note_id,note_content,author){
+
+	 	
+
+		for(i=0; i<notes.length; i++){
+
+		}
+
+	}
