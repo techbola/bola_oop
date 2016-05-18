d199f55881a1c308e7093090c856b31abdf3d196
bola <bfagbowore@gmail.com>
2016-05-18 18:00:35 +0100

    Modified the whole code to return the desired output

---
+++ working_oop.js
@@ -0,0 +1,40 @@
+function NotesApplication (author){
+	this.author = author;
+	this.notes =  [];
+    
+}
+	NotesApplication.prototype.create = function (note_content){
+		return this.notes.push(note_content);
+		
+	};
+	
+	NotesApplication.prototype.get = function (note_id){
+		var gt = this.notes[note_id];
+		console.log(gt);
+	};
+	
+	NotesApplication.prototype.listNotes =function (){
+		for(var i=0; i<this.notes.length; i++){
+			console.log("Note ID: " + i );
+			console.log("Note Content: " + this.notes[i]);
+			console.log("Author: " + this.author );
+		}
+	};
+
+	NotesApplication.prototype.search = function(search_text) {
+		var result ='';
+		for (var i = 0; i < this.notes.length; i++) {
+			if(this.notes[i].indexOf(search_text)){
+			console.log("Showing result for search:" + " " + "Note ID: " + i + this.notes[i] + "By Author " + this.author)
+			}
+		return result;
+	}
+	};
+	
+	NotesApplication.prototype.delete = function(note_id) {
+		delete this.notes[note_id - 1];
+	};
+	
+	NotesApplication.prototype.edit = function(note_id, new_content){
+		return this.notes[note_id]= new_content;
+	};
